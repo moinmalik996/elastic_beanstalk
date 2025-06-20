7fe747a1b85d130852cc53915e8dcb2ebe36551c
Moin Abbas <moin.malik996@gmail.com>
2025-06-20 05:10:10 +0100

    some changes in the project

--- elastic_beanstalk/settings.py
+++ elastic_beanstalk/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'elastic_beanstalk.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("NAME"),
-        'USER': os.environ.get("USER"),
-        'PASSWORD': os.environ.get("PASSWORD"),
-        'HOST': os.environ.get("HOST"),
-        'PORT': os.environ.get("PORT")
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT")
     }
 }
 
